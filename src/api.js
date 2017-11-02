dd60c39cad8749ec3aff46daf3adaee2144e7141
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-11-02 22:26:58 +0530

    http api testing

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const build = true;
 
-const api = build ? 'https://35.165.95.161:4000' : 'http://127.0.0.1:4000';
+const api = build ? 'http://35.165.95.161:4000' : 'http://127.0.0.1:4000';
 
 export default api;
